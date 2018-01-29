372daee94d88410ddb82352e9af7245c0a528b48
Flolight <clanet@etud.insa-toulouse.fr>
2018-01-29 15:33:44 +0100

    images config

--- toposdci.py
+++ toposdci.py
@@ -8,11 +8,11 @@ class TopoSdci (Topo):
 		Topo.__init__(self)
 		
 		gi  = self.addDocker('gi',  ip='10.0.0.3', dimage="flolight/gi")
-		#gf1 = self.addDocker('gf1', ip='10.0.0.4', dimage="flolight/gf1")
+		gf1 = self.addDocker('gf1', ip='10.0.0.4', dimage="flolight/gf1")
 		#gf2 = self.addDocker('gf2', ip='10.0.0.5', dimage="flolight/gf2")
 		#gf3 = self.addDocker('gf3', ip='10.0.0.6', dimage="flolight/gf3")
 
-		appserver = self.addDocker('appserver', ip='10.0.0.2', dimage="sdciproject/appserver")
+		appserver = self.addDocker('appserver', ip='10.0.0.2', dimage="flolight/appserver")
 
 		s1  = self.addSwitch( 's1' )
 		
