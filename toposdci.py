3873d648900596e0479ef860c90b597eade18e9d
Flolight <clanet@etud.insa-toulouse.fr>
2018-01-31 12:10:25 +0100

    topo

--- toposdci.py
+++ toposdci.py
@@ -13,18 +13,20 @@ class TopoSdci (Topo):
 		#gi  = self.addDocker('gi',  ip='10.0.0.3', dimage="ubuntu:trusty")
 		#gf1 = self.addDocker('gf1', ip='10.0.0.4', dimage="ubuntu:trusty")
 		
+		appserver = self.addDocker('appserver', ip='10.0.0.2', dimage="flolight/appserver")
 
 		gi  = self.addDocker('gi',  ip='10.0.0.3', dimage="flolight/gi")
 		gf1 = self.addDocker('gf1', ip='10.0.0.4', dimage="flolight/gf1")
 		gf2 = self.addDocker('gf2', ip='10.0.0.5', dimage="flolight/gf2")
 		gf3 = self.addDocker('gf3', ip='10.0.0.6', dimage="flolight/gf3")
 
-		appserver = self.addDocker('appserver', ip='10.0.0.2', dimage="flolight/appserver")
-
+		
 		s1  = self.addSwitch( 's1' )
 
-		self.addLink(gf1, gi)
-		self.addLink(gi, gf1)
-		self.addLink(appserver, gi)
+		self.addLink(gf1, s1)
+		self.addLink(gf2, s1)
+		self.addLink(gf3, s1)
+		self.addLink(gi, s1)
+		self.addLink(appserver, s1)
 		
 topos = { 'toposdci': ( lambda: TopoSdci() ) }
