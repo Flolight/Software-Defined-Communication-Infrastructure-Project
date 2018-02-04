63a25aed8ff2582d2096cd67b4031aad7ce73d80
Yuxiao MAO <0210youjing@gmail.com>
2018-02-04 03:45:31 +0100

    add routingrule addition rule

--- SDCI_Project/src/sdnAdapter/DeviceRest.java
+++ SDCI_Project/src/sdnAdapter/DeviceRest.java
@@ -5,8 +5,11 @@ import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -18,64 +21,92 @@ public class DeviceRest {
 	}
 	
 	// Network setting
-	public static final String STATIC_ENTRY_PUSHER_URL = "http://localhost:8080/wm/staticentrypusher/json";
+	public static final String LOCAL_IPV4 = "localhost";
+	public static final String STATIC_ENTRY_PUSHER_URL = "http://"+LOCAL_IPV4+":8080/wm/staticentrypusher/json";
 	
 	public static final String SERVER_IPV4 = "10.0.0.2";
+	public static final String SERVER_SWITCH_PORT = "5";
 	public static final String GINIT_IPV4 = "10.0.0.3";
+	public static final String GINIT_SWITCH_PORT = "4";
 	public static final String GF1_IPV4 = "10.0.0.4";
+	public static final String GF1_SWITCH_PORT = "1";
 	public static final String GF2_IPV4 = "10.0.0.5";
+	public static final String GF2_SWITCH_PORT = "2";
 	public static final String GF3_IPV4 = "10.0.0.6";
-	public static final String DC_IPV4 = "10.0.0.8";
-	public static final String SWITCH1_IPV4 = "10.0.2.15:35398";
-	public static final String SWITCH1_ID = "00:00:00:00:00:00:00:01";
-	public static final String SWITCH2_IPV4 = "10.0.2.15:35400";
-	public static final String SWITCH2_ID = "00:00:00:00:00:00:00:02";
+	public static final String GF3_SWITCH_PORT = "3";
 	
-	public static String sendPost(String url, JSONObject jsonObj) {
+	public static final String DC_IPV4 = "10.0.0.8";
+	public static final String DC_SWITCH_PORT = "2";
+	
+	public static final String SWITCH1_IPV4 = LOCAL_IPV4+"35398";
+	public static final String SWITCH1_ID = "00:00:00:00:00:00:00:01";
+	public static final String SWITCH1_TO_SWITCH2_PORT = "6";
+	
+	public static final String SWITCH2_IPV4 = LOCAL_IPV4+"35400";
+	public static final String SWITCH2_ID = "00:00:00:00:00:00:00:02";
+	public static final String SWITCH2_TO_SWITCH1_PORT = "1";
+	
+	public static final String METHOD_POST = "POST";
+	public static final String METHOD_GET = "GET";
+	public static final String METHOD_DELETE = "DELETE";
+	
+	
+	public static String send(String method, String url, JSONObject jsonObj) {
 		HttpClient client = HttpClientBuilder.create().build();
-		HttpPost post = new HttpPost(url);
-
+		
+		HttpRequestBase request;
+		if (method.equalsIgnoreCase(METHOD_POST)) {
+			request = new HttpPost(url);
+		}else if (method.equalsIgnoreCase(METHOD_GET)) {
+			request = new HttpGet(url);
+		}else if (method.equalsIgnoreCase(METHOD_DELETE)) {
+			request = new HttpDelete(url);
+		}else {
+			System.out.println("[DeviceRest]Unknown method");
+			return null;
+		}
+		
+		
+		String data = null;
 		try {
-			StringEntity entity = new StringEntity(jsonObj.toString());
-			entity.setContentType("application/json");
-			entity.setContentEncoding("UTF-8");
-			post.setEntity(entity);
-			HttpResponse response = client.execute(post);
+			if (jsonObj != null) {
+				// for POST only
+				StringEntity entity = new StringEntity(jsonObj.toString());
+				entity.setContentType("application/json");
+				entity.setContentEncoding("UTF-8");
+				((HttpEntityEnclosingRequestBase) request).setEntity(entity);
+			}
 			
-			return EntityUtils.toString(response.getEntity());
+			HttpResponse response = client.execute(request);
+			data = EntityUtils.toString(response.getEntity());
 			
 		} catch (UnsupportedEncodingException e1) {
 			e1.printStackTrace();
 		}catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			post.releaseConnection();
+			request.releaseConnection();
 		}
 		
-		return null;
-	}
-	
-	public static String sendGet(String url) {
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpGet get = new HttpGet(url);
-
-		try {
-			HttpResponse response = client.execute(get);
-			
-			return EntityUtils.toString(response.getEntity());
-		}catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			get.releaseConnection();
-		}
-		
-		return null;
+		return data;
 	}
 	
 	private static int nameCounter = 0;
-	public static JSONObject createRequestObject(String targetSwitchId, String inPort,
-			String ipSrc, String ipDst, String setEthDst, String outPort) {
+	public static JSONObject createRequestObject(int switchNumber, String inPort,
+			String ipSrc, String ipDst, String outPort) {
 		JSONObject obj = new JSONObject();
+		
+		String targetSwitchId;
+		switch (switchNumber) {
+		case 1:
+			targetSwitchId = DeviceRest.SWITCH1_ID;
+			break;
+		case 2:
+			targetSwitchId = DeviceRest.SWITCH2_ID;
+			break;
+		default:
+			targetSwitchId = "";
+		}
 		obj.put("switch", targetSwitchId);
 		obj.put("name", "flow-"+(nameCounter++));
 		obj.put("priority", "36000");
@@ -83,9 +114,7 @@ public class DeviceRest {
 		obj.put("eth_type", "0x0800");
 		obj.put("ipv4_src", ipSrc);
 		obj.put("ipv4_dst", ipDst);
-		obj.put("actions",
-				"set_field=eth_dst->"+setEthDst+","
-				+ "output="+outPort);
+		obj.put("actions", "output="+outPort);
 		
 		return obj;
 	}
--- SDCI_Project/src/sdnAdapter/SDNAdapter.java
+++ SDCI_Project/src/sdnAdapter/SDNAdapter.java
@@ -4,6 +4,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 import model.Address;
 import model.DataType_CT;
 import model.DataType_GF;
@@ -21,30 +23,99 @@ public class SDNAdapter {
 	private final ArrayList<DataType_RoutingRule> routingRules;
 	
 	public SDNAdapter() {
-		// TODO Auto-generated constructor stub
 		this.routingRules = new ArrayList<>();
 	}
 
 	public void createRoutingRule(DataType_RoutingRule rule) {
-		// TODO test different case and add rules to switch
-		Address adrSrc = rule.getSrcIPAddress();
-		switch (adrSrc.getIpAdress().toString()) {
-		case DeviceRest.GF1_IPV4:
-			break;
-		case DeviceRest.GF2_IPV4:
-			break;
-		case DeviceRest.GF3_IPV4:
-			break;
-		case DeviceRest.SERVER_IPV4:
-			Address adrDst = rule.getDestIPAddress();
-			switch(adrDst.getIpAdress().toString()) {
-			case DeviceRest.GF1_IPV4:
-				break;
-			case DeviceRest.GF2_IPV4:
-				break;
-			case DeviceRest.GF3_IPV4:
-				break;
-			}
+		String adrSrc = rule.getSrcIPAddress().getIpAdress().toString();
+		Address adrRoute = rule.getAdrRoute();
+		String ipGF = "";
+		String swPortGF = "";
+		if (adrSrc.contains(DeviceRest.GF1_IPV4)){
+			ipGF = DeviceRest.GF1_IPV4;
+			swPortGF = DeviceRest.GF1_SWITCH_PORT;
+		}else if (adrSrc.contains(DeviceRest.GF2_IPV4)){
+			ipGF = DeviceRest.GF2_IPV4;
+			swPortGF = DeviceRest.GF2_SWITCH_PORT;
+		}else if (adrSrc.contains(DeviceRest.GF3_IPV4)){
+			ipGF = DeviceRest.GF2_IPV4;
+			swPortGF = DeviceRest.GF2_SWITCH_PORT;
+		}else {
+			System.out.println("[SDNAdapter]Unknow GF ipv4:"+adrSrc);
+		}
+		
+		
+		if (adrRoute.getIpAdress().toString().equals(DeviceRest.GINIT_IPV4)) {
+			// if adrRoute == Gateway Init
+			// ------ GF to Server ------
+			// for sw1, flow from GFx goes to GI
+			JSONObject obj1 = DeviceRest.createRequestObject(1, swPortGF, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.GINIT_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj1);
+			// for sw1, flow from GI goes to Server
+			JSONObject obj2 = DeviceRest.createRequestObject(1, DeviceRest.GINIT_SWITCH_PORT, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.SERVER_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj2);
+			
+			// ------ Server to GF ------
+			// for sw1, flow from Server goes to GI
+			JSONObject obj3 = DeviceRest.createRequestObject(1, DeviceRest.SERVER_SWITCH_PORT, DeviceRest.SERVER_IPV4,
+					ipGF, DeviceRest.GINIT_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj3);
+			// for sw1, flow from GI goes to GFx
+			JSONObject obj4 = DeviceRest.createRequestObject(1, DeviceRest.GINIT_SWITCH_PORT, DeviceRest.SERVER_IPV4,
+					ipGF, swPortGF);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj4);
+			
+		}else {
+			// if adrRoute in DC
+			// ------ GF to Server ------
+			// for sw1, flow from GFx goes to SW2
+			JSONObject obj1 = DeviceRest.createRequestObject(1, swPortGF, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.SWITCH1_TO_SWITCH2_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj1);
+			
+			// for sw2, flow from SW1 goes to DC; DST became GI
+			JSONObject obj2 = DeviceRest.createRequestObject(2,
+					DeviceRest.SWITCH2_TO_SWITCH1_PORT, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.DC_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj2);
+			
+			// for sw2, flow from GI goes to SW1; DST became Server
+			JSONObject obj3 = DeviceRest.createRequestObject(2,
+					DeviceRest.DC_SWITCH_PORT, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.SWITCH2_TO_SWITCH1_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj3);
+			
+			// for sw1, flow from SW2 goes to Server
+			JSONObject obj4 = DeviceRest.createRequestObject(1,
+					DeviceRest.SWITCH1_TO_SWITCH2_PORT, ipGF,
+					DeviceRest.SERVER_IPV4, DeviceRest.SERVER_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj4);
+			
+			// ------ Server to GF ------
+			// for sw1, flow from Server goes to SW2
+			JSONObject obj5 = DeviceRest.createRequestObject(1, swPortGF, DeviceRest.SERVER_IPV4,
+					ipGF, DeviceRest.SWITCH1_TO_SWITCH2_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST,DeviceRest.STATIC_ENTRY_PUSHER_URL, obj5);
+			
+			// for sw2, flow from SW1 goes to DC; DST became GI
+			JSONObject obj6 = DeviceRest.createRequestObject(2,
+					DeviceRest.SWITCH2_TO_SWITCH1_PORT, DeviceRest.SERVER_IPV4,
+					ipGF, DeviceRest.DC_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj6);
+			
+			// for sw2, flow from DC goes to SW1; DST became GFx
+			JSONObject obj7 = DeviceRest.createRequestObject(2,
+					DeviceRest.DC_SWITCH_PORT, DeviceRest.SERVER_IPV4,
+					ipGF, DeviceRest.SWITCH2_TO_SWITCH1_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj7);
+			
+			// for sw1, flow from SW2 goes to GFx
+			JSONObject obj8 = DeviceRest.createRequestObject(1,
+					DeviceRest.SWITCH1_TO_SWITCH2_PORT, DeviceRest.SERVER_IPV4,
+					ipGF, DeviceRest.SERVER_SWITCH_PORT);
+			DeviceRest.send(DeviceRest.METHOD_POST, DeviceRest.STATIC_ENTRY_PUSHER_URL, obj8);
 		}
 		
 		this.routingRules.add(rule);
